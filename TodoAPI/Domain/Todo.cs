11857cb4caf12cb1fc8072cf084b87fce2daeb08
Ygor Amaral <ygor_amaral96@hotmail.com>
2021-07-21 00:36:06 -0300

    Adicionado model de User e Todo mais conteúdos da build

---
+++ TodoAPI/Domain/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Domain
+{
+    public class Todo
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public int? Order { get; set; }
+
+    }
+}
